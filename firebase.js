e84c3a796120515519158ee57d1406e3c8836b72
shahriar bhuiyan <shahriarbhuiyan1@gmail.com>
2023-10-20 14:19:14 +0600

    securitydone

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBYbm95K1Lou7SF6gPPIk0Wlbawl4t8lk",
-  authDomain: "laundry-application-39fdf.firebaseapp.com",
-  projectId: "laundry-application-39fdf",
-  storageBucket: "laundry-application-39fdf.appspot.com",
-  messagingSenderId: "52926193121",
-  appId: "1:52926193121:web:6e5ef9b2f46accfa74c6a0"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: Process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
